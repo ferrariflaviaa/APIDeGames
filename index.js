1b406d63b88a52487f9fae2db613b7567dd4b067
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-10-19 15:40:56 -0300

    feat: main routes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express();
 const bodyParser = require("body-parser")
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
 let DB = {
@@ -27,6 +27,89 @@ let DB = {
     }
   ]
 }
+
+app.get('/games', (req, res) => {
+  res.statusCode = 200
+  res.json(DB.games);
+});
+
+app.get("/games/:id", (req, res) => {
+  const { id } = req.params
+  if (isNaN(id)) {
+    res.sendStatus(400)
+  } else {
+    let idGame = parseInt(id);
+    let game = DB.games.find(g => g.id == idGame)
+    if (game !== undefined) {
+      res.statusCode = 200
+      res.json(game)
+    } else {
+      res.sendStatus(404)
+    }
+  }
+})
+
+app.post("/game", (req, res) => {
+  //Cadastrando games
+  const { title, price, year } = req.body;
+  if (title && price && year) {
+    DB.games.push({
+      id: 112,
+      title,
+      price,
+      year
+    })
+    res.sendStatus(200);
+  }
+})
+
+app.delete("/game/:id", (req, res) => {
+  //Deletação
+  const { id } = req.params;
+
+  if (isNaN(id)) {
+    res.sendStatus(400)
+  } else {
+    let idGames = parseInt(id);
+    let index = DB.games.findIndex(g => g.id === idGames)
+
+    if (index == -1) {
+      res.sendStatus(404)
+    } else {
+      DB.games.splice(index, 1);
+      res.sendStatus(200)
+    }
+  }
+})
+
+app.put("/game/:id", (req, res) => {
+  //Edição
+  const { title, price, year } = req.body;
+  if (isNaN(id)) {
+    res.sendStatus(400)
+  } else {
+    let idGame = parseInt(id);
+    let game = DB.games.find(g => g.id == idGame)
+    if (game !== undefined) {
+
+      if(title != undefined){
+        game.title = title
+      }
+
+      if(price != undefined){
+        game.price = price
+      }
+
+      if(year != undefined){
+        game.year = year
+      }
+
+      res.statusCode = 200
+    } else {
+      res.sendStatus(404)
+    }
+  }
+})
 app.listen(45678, () => {
   console.log("API RODANDO!!")
 })
