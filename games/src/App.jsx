be3bb3ab9b9ee61f0d48b080acc70f1ba3de16ed
ferrariflaviaa <flaviaferrarit@gmail.com>
2023-10-20 14:20:43 -0300

    style: formatting the boss stylizations

--- games/src/App.jsx
+++ games/src/App.jsx
@@ -1,7 +1,23 @@
-import "./App.css";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+// import { Header } from "./components/header";
+// import { Footer } from "./components/sessions/footer";
+import { Pages } from "./pages";
+import { GlobalStyles } from "./styles/globalStyles";
+import themes from "./themes";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <ThemeProvider theme={themes}>
+        {/* <Header /> */}
+        <GlobalStyles />
+        <Pages />
+        {/* <Footer /> */}
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ games/src/themes/index.ts
@@ -0,0 +1,9 @@
+export default {
+  COLORS: {
+    WHITE: "#FFFFFF",
+    BGCOLOR1: "#282A2D",
+    BGCOLOR2: "#2A3647",
+    TEXTCOLOR1: "#485C7A",
+    BGCOLOR3: "#B1BAC7",
+  },
+};
